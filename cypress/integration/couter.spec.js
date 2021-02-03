f2eac52d87a0a9e5559a3b728d38db046007d911
yujo <yujo@student.42seoul.kr>
2021-02-03 17:08:20 +0900

    feat: counter  plus, minus 기능 구현

--- cypress/integration/couter.spec.js
+++ cypress/integration/couter.spec.js
@@ -1,45 +1,45 @@
-describe('ui-counter', () => {
+describe("ui-counter", () => {
   beforeEach(() => {
     // 페이지 접속. 띄워진 서버 port를 작성해주세요.
-    cy.visit('http://localhost:5500/');
+    cy.visit("http://localhost:5500/");
   });
 
-  it('생성시 버튼과 초기값(10)을 렌더링 한다', () => {
+  it("생성시 버튼과 초기값(10)을 렌더링 한다", () => {
     // count-display 클래스를 가진 요소의 텍스트가 10
-    cy.get('.count-display').should('have.value', '10');
+    cy.get(".count-display").should("have.value", "10");
   });
 
-  it('+ 버튼 클릭시 1 증가한다.', () => {
+  it("+ 버튼 클릭시 1 증가한다.", () => {
     // value 클래스를 가진 요소의 텍스트가 10
-    const prevInputValue = cy.get('.count-display');
+    const prevInputValue = cy.get(".count-display");
 
     // btn-inc 클래스를 가진 요소를 클릭
-    cy.get('.plus-button').click();
-    cy.get('.count-display').should('have.value', +prevInputValue + 1);
+    cy.get(".plus-button").click();
+    cy.get(".count-display").should("have.value", +prevInputValue + 1);
   });
 
-  it('- 버튼 클릭시 1 증가한다.', () => {
+  it("- 버튼 클릭시 1 증가한다.", () => {
     // value 클래스를 가진 요소의 텍스트가 10
-    const prevInputValue = cy.get('.count-display');
+    const prevInputValue = cy.get(".count-display");
 
     // btn-inc 클래스를 가진 요소를 클릭
-    cy.get('.minus-button').click();
-    cy.get('.count-display').should('have.value', +prevInputValue - 1);
+    cy.get(".minus-button").click();
+    cy.get(".count-display").should("have.value", +prevInputValue - 1);
   });
 
-  it('count max값이 12이다.', () => {
-    const prevInputValue = cy.get('.count-display');
+  it("count max값이 12이다.", () => {
+    const prevInputValue = cy.get(".count-display");
 
     // btn-inc 클래스를 가진 요소를 클릭
-    cy.get('.minus-button').click();
-    cy.get('.count-display').should('have.value.of.at.most', 13);
+    cy.get(".minus-button").click();
+    cy.get(".count-display").should("have.value.of.at.most", 13);
   });
 
-  it('count min값이 8이다.', () => {
-    const prevInputValue = cy.get('.count-display');
+  it("count min값이 8이다.", () => {
+    const prevInputValue = cy.get(".count-display");
 
     // btn-inc 클래스를 가진 요소를 클릭
-    cy.get('.minus-button').click();
-    cy.get('.count-display').should('have.value.of.at.least', 8);
+    cy.get(".minus-button").click();
+    cy.get(".count-display").should("have.value.of.at.least", 8);
   });
 });
--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,24 @@ export default function App() {
 }
 
 App();
+
+const $plusButton = document.querySelector(".plus-button");
+const $minusButton = document.querySelector(".minus-button");
+const $countDisplay = document.querySelector(".count-display");
+
+const increaseValue = () => {
+  if ($countDisplay.value >= 12) {
+    return;
+  }
+  $countDisplay.value = Number($countDisplay.value) + 1;
+};
+
+const decreaseValue = () => {
+  if ($countDisplay.value <= 8) {
+    return;
+  }
+  $countDisplay.value = Number($countDisplay.value) - 1;
+};
+
+$plusButton.addEventListener("click", increaseValue);
+$minusButton.addEventListener("click", decreaseValue);
